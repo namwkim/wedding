225d55a7f6be3f7fe0a13f5789c15e7df0949a87
Nam Wook Kim <namwkim85@gmail.com>
2017-08-05 00:54:26 +0900

    draft

---
+++ index.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+  //initialize
+  // console.log('screen.height', window.innerHeight);
+
+}
+
+
+
+new Vue({
+  el: '#image-list',
+  data: {
+    images:[
+      'img/photo1.jpeg',
+      'img/photo2.jpeg',
+      'img/photo3.jpeg',
+      'img/photo4.png',
+      'img/photo5.jpeg',
+      'img/photo6.jpeg',
+      'img/photo7.jpeg',
+      'img/photo5.jpeg',
+      'img/photo5.jpeg'
+    ]
+  },
+  methods:{
+    showImage:function(image){
+      document.getElementById('popup').style.display = 'block';
+      document.getElementById('popup-content').src = image;
+      document.body.style.overflow = 'hidden';
+    }
+  }
+})
+
+new Vue({
+  el:'#popup',
+  methods:{
+    close: function(event){
+      if (event.target===event.currentTarget){
+        document.getElementById('popup').style.display='none';
+        document.body.style.overflow = '';
+      }
+
+    }
+  }
+})

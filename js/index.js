aed2dc5f3937a625096acadeb4fb7d4ea6bc8011
Nam Wook Kim <namwkim85@gmail.com>
2017-08-06 00:45:22 +0900

    update

---
+++ js/index.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+  //initialize
+  // console.log('screen.height', window.innerHeight);
+  var tc = document.getElementsByClassName('teaser-container')[0];
+  tc.style.opacity = 1.0;
+  tc.style.height = window.innerHeight + 'px';
+
+  // add animations
+  var ti = document.getElementsByClassName('teaser-image')[0];
+  ti.classList.add('animated', 'fadeIn');
+
+  var list = document.getElementsByClassName('teaser-text');
+  for (var i=0; i<list.length; i++){
+      list[i].classList.add('animated', 'fadeInUp');
+  }
+}
+
+function showImage(image){
+  document.getElementById('popup').style.display = 'block';
+  document.getElementById('popup').classList.remove('animated', 'fadeOut');
+  document.getElementById('popup').classList.add('animated', 'fadeIn');
+  // document.getElementById('popup-content').classList.add('animated', 'flipInX');
+  document.getElementById('popup-image').src = image;
+  document.body.style.overflow = 'hidden';
+}
+
+function closePopup(event){
+  if (event.target===event.currentTarget){
+    document.getElementById('popup').classList.remove('animated', 'fadeIn');
+    document.getElementById('popup').classList.add('animated', 'fadeOut');
+    // document.getElementById('popup').style.display='none';
+    // document.getElementById('popup-content').classList.remove('animated', 'flipInX');
+    document.body.style.overflow = '';
+  }
+}
